9164fa3f74413b3e446b30c639905e65a574a9d0
AdriOlogia <eidrian747@gmail.com>
2023-06-22 14:51:45 -0300

    se agregaron nuevos efectos

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,11 @@ const scrollFunction = () => {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.getElementById("AdrianBrand").style.fontSize = "1.25rem";
       document.getElementById("firstNav").style.background = 'linear-gradient(180deg, rgba(0,0,0,1) 20%, rgba(0,0,0,0.25) 100%)';
-
+      document.getElementById("hiddenbtn").style.background = "white"
     } else {
       document.getElementById("AdrianBrand").style.fontSize = "2.25rem";
       document.getElementById("firstNav").style.background = "transparent" ;
+      document.getElementById("hiddenbtn").style.background = "transparent";
 
     }
   }

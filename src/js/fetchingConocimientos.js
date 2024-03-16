5c40534616826d5f0284120cca42acb718b13f5f
AdriOlogia <eidrian747@gmail.com>
2024-03-16 13:54:34 -0300

    se agiliza los skills mediante script y se ajustan diseños de botones

--- src/js/fetchingConocimientos.js
+++ src/js/fetchingConocimientos.js
@@ -1,15 +1,29 @@
 import Conocimientos from '../helpers/Conocimientos.json'
+import ScrollReveal from 'scrollreveal'
 
 let skill_box = document.getElementById("skills-content")
 
-
 for (const skills in Conocimientos) {
     if (Conocimientos.hasOwnProperty(skills)) {
-        console.log(Conocimientos[skills].img)
+        skill_box.innerHTML += `
+        <div class="col-lg-2 m-lg-4 col-md-3 m-md-4 col-sm-3 m-sm-2 col-4 m-2 p-2 cards">
+            <img src="${Conocimientos[skills].img}" alt="${skills}">
+        </div>
+        `
+    }
+}
+
+const node_content_section3 = document.querySelector(".content-section-3")
+const node_img_section3 = node_content_section3.querySelectorAll("img")
+
+ScrollReveal().reveal(node_img_section3, {
+    delay: 700,
+    reset: true,
+    easing: 'ease-in',
+    origin: 'bottom',
+    rotate: {
+        x: 20,
+        z: 20
     }
 
-    skill_box.innerHTML = `
-        <div class="col-lg-2 m-lg-4 col-md-3 m-md-4 col-sm-3 m-sm-2 col-4 m-2 p-2 cards">
-            <img src="${Conocimientos[skills].img}">${skills}</p>
-        </div>`
-}
+});
